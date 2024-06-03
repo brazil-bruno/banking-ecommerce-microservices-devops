446b8f6f3ce4b58d3182ab502e179e4249d4d80d
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-03 17:28:02 -0300

    AccountController created

---
+++ banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountController.java
@@ -0,0 +1,14 @@
+package com.bruno.microservices.account.controllers;
+
+import com.bruno.microservices.account.services.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+}
