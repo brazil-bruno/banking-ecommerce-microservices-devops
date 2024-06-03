2961f7f92713ad76f2cbe896f1f2ffaf336a527f
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-03 17:50:11 -0300

    findAllAccounts, createNewAccount, findAccountById, updateAccount and deleteAccountById methods created

--- banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountController.java
+++ banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountController.java
@@ -1,9 +1,12 @@
 package com.bruno.microservices.account.controllers;
 
+import com.bruno.microservices.account.entities.Account;
 import com.bruno.microservices.account.services.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -11,4 +14,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountService accountService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Account> findAllAccounts() {
+        return accountService.findAllAccounts();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Account createNewAccount(@RequestBody Account account) {
+        return accountService.createNewAccount(account);
+    }
+
+    @GetMapping("/{accountID}")
+    @ResponseStatus(HttpStatus.OK)
+    public Account findAccountById(@PathVariable String accountID) {
+        return accountService.findAccountById(accountID);
+    }
+
+    @PutMapping("/{accountID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Account updateAccount(@RequestBody Account account, @PathVariable String accountID) {
+        return accountService.updateAccount(account, accountID);
+    }
+
+    @DeleteMapping("/{accountID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAccountById(@PathVariable String accountID) {
+        accountService.deleteAccountById(accountID);
+    }
 }
--- banking/account/src/main/java/com/bruno/microservices/account/services/AccountService.java
+++ banking/account/src/main/java/com/bruno/microservices/account/services/AccountService.java
@@ -1,12 +1,56 @@
 package com.bruno.microservices.account.services;
 
+import com.bruno.microservices.account.entities.Account;
 import com.bruno.microservices.account.repositories.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
     private final AccountRepository accountRepository;
+
+    public List<Account> findAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account createNewAccount(Account account) {
+        Account entity = Account.builder()
+                .accountID(UUID.randomUUID().toString())
+                .accountNumber(accountNumberGenerate())
+                .accountBalance(0.0)
+                .accountPassword(account.getAccountPassword())
+                .build();
+        return accountRepository.save(entity);
+    }
+
+    public Account findAccountById(String accountID) {
+        return accountRepository.findById(accountID).get();
+    }
+
+    public Account updateAccount(Account account, String accountID) {
+        Account entity = accountRepository.findById(accountID).get();
+        entity.setAccountPassword(account.getAccountPassword());
+        return accountRepository.save(entity);
+    }
+
+    public void deleteAccountById(String accountID) {
+        accountRepository.deleteById(accountID);
+    }
+
+    private String accountNumberGenerate() {
+        Random rand = new Random();
+        String accountNumber = "0462";
+        for (int i = 0; i < 8; i++)
+        {
+            int n = rand.nextInt(10) + 0;
+            accountNumber += Integer.toString(n);
+        }
+        return accountNumber;
+    }
 }
