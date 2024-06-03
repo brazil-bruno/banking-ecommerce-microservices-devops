d6704e86ef971e5ef74186ba18697a100bb09a58
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-03 15:57:45 -0300

    findAllAddresses, createNewAddress, findAddressById, updateAddress and deleteAddressById methods created

--- banking/address/src/main/java/com/bruno/microservices/address/controllers/AddressController.java
+++ banking/address/src/main/java/com/bruno/microservices/address/controllers/AddressController.java
@@ -1,9 +1,12 @@
 package com.bruno.microservices.address.controllers;
 
+import com.bruno.microservices.address.entities.Address;
 import com.bruno.microservices.address.services.AddressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/addresses")
@@ -11,4 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddressController {
 
     private final AddressService addressService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Address> findAllAddresses() {
+        return addressService.findAllAddresses();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Address createNewAddress(@RequestBody Address address) {
+        return addressService.createNewAddress(address);
+    }
+
+    @GetMapping("/{addressID}")
+    public Address findAddressById(@PathVariable String addressID) {
+        return addressService.findAddressById(addressID);
+    }
+
+    @PutMapping("/{addressID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Address updateAddress(@RequestBody Address address, @PathVariable String addressID) {
+        return addressService.updateAddress(address, addressID);
+    }
+
+    @DeleteMapping("/{addressID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAddressById(@PathVariable String addressID) {
+        addressService.deleteAddressById(addressID);
+    }
 }
--- banking/address/src/main/java/com/bruno/microservices/address/services/AddressService.java
+++ banking/address/src/main/java/com/bruno/microservices/address/services/AddressService.java
@@ -1,12 +1,54 @@
 package com.bruno.microservices.address.services;
 
+import com.bruno.microservices.address.entities.Address;
 import com.bruno.microservices.address.repositories.AddressRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AddressService {
 
     private final AddressRepository addressRepository;
+
+    public List<Address> findAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    public Address createNewAddress(Address address) {
+        Address entity = Address.builder()
+                .addressID(UUID.randomUUID().toString())
+                .publicArea(address.getPublicArea())
+                .addressNumber(address.getAddressNumber())
+                .complement(address.getComplement())
+                .neighborhood(address.getNeighborhood())
+                .zipCode(address.getZipCode())
+                .city(address.getCity())
+                .state(address.getState())
+                .build();
+        return addressRepository.save(entity);
+    }
+
+    public Address findAddressById(String addressID) {
+        return addressRepository.findById(addressID).get();
+    }
+
+    public Address updateAddress(Address address, String addressID) {
+        Address entity = addressRepository.findById(addressID).get();
+        entity.setPublicArea(address.getPublicArea());
+        entity.setAddressNumber(address.getAddressNumber());
+        entity.setComplement(address.getComplement());
+        entity.setNeighborhood(address.getNeighborhood());
+        entity.setZipCode(address.getZipCode());
+        entity.setCity(address.getCity());
+        entity.setState(address.getState());
+        return addressRepository.save(entity);
+    }
+
+    public void deleteAddressById(String addressID) {
+        addressRepository.deleteById(addressID);
+    }
 }
