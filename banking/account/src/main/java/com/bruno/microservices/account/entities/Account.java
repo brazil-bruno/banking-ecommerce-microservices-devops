28783c01e371223d7998335bd6525eccb7be3f34
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-03 17:26:47 -0300

    Account Entity created

---
+++ banking/account/src/main/java/com/bruno/microservices/account/entities/Account.java
@@ -0,0 +1,29 @@
+package com.bruno.microservices.account.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_account")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Account {
+
+    @Id
+    private String accountID;
+
+    private String accountNumber;
+
+    private double accountBalance;
+
+    private String accountPassword;
+
+}
