c70479bbd0309a4c0b54279ed3c0da0de890cf9c
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-05 10:22:58 -0300

    ClientFeignClient created

---
+++ banking/account/src/main/java/com/bruno/microservices/account/feignclients/ClientFeignClient.java
@@ -0,0 +1,13 @@
+package com.bruno.microservices.account.feignclients;
+
+import com.bruno.microservices.account.entities.Client;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "client", url = "${client.url}")
+public interface ClientFeignClient {
+
+    @GetMapping("/api/clients/{clientID}")
+    Client findClientById(@PathVariable String clientID);
+}
