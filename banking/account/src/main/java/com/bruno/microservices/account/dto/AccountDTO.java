466d733578d3cb3d8cea3f99ba9e49a003ab9c57
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-05 10:22:14 -0300

    AccountDTO created

---
+++ banking/account/src/main/java/com/bruno/microservices/account/dto/AccountDTO.java
@@ -0,0 +1,13 @@
+package com.bruno.microservices.account.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AccountDTO {
+
+    private String accountPassword;
+}
