36206c5e6dd2b7e47a97f55cb6f75a174306600c
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-10 16:02:25 -0300

    String Id changed to UUID

--- banking/account/src/main/java/com/bruno/microservices/account/feignclients/ClientFeignClient.java
+++ banking/account/src/main/java/com/bruno/microservices/account/feignclients/ClientFeignClient.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
+
 @FeignClient(value = "client", url = "${client.url}")
 public interface ClientFeignClient {
 
     @GetMapping("/api/clients/{clientID}")
-    Client findClientById(@PathVariable String clientID);
+    Client findClientById(@PathVariable UUID clientID);
 }
