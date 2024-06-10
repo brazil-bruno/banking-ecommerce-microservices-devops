c137499bb8676b577d159e4f5980a5129b9bf6ca
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-10 18:26:09 -0300

    updateProduct added

--- ecommerce/order/src/main/java/com/bruno/microservice/order/feignclients/ProductFeignClient.java
+++ ecommerce/order/src/main/java/com/bruno/microservice/order/feignclients/ProductFeignClient.java
@@ -2,8 +2,8 @@ package com.bruno.microservice.order.feignclients;
 
 import com.bruno.microservice.order.entities.Product;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -11,5 +11,10 @@ import java.util.UUID;
 public interface ProductFeignClient {
 
     @GetMapping("/api/products/{productID}")
+    @ResponseStatus(HttpStatus.OK)
     Product findProductById(@PathVariable UUID productID);
+
+    @PutMapping("/api/products/{productID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    Product updateProduct(@RequestBody Product product, @PathVariable UUID productID);
 }
