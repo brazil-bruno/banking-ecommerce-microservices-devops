74b303ed74d583cc4bc7338ac593c041ee352121
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-10 15:04:18 -0300

    addressID changed to UUID type

--- banking/address/src/main/java/com/bruno/microservices/address/repositories/AddressRepository.java
+++ banking/address/src/main/java/com/bruno/microservices/address/repositories/AddressRepository.java
@@ -4,6 +4,8 @@ import com.bruno.microservices.address.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface AddressRepository extends JpaRepository<Address, String> {
+public interface AddressRepository extends JpaRepository<Address, UUID> {
 }
--- banking/address/src/main/java/com/bruno/microservices/address/services/AddressService.java
+++ banking/address/src/main/java/com/bruno/microservices/address/services/AddressService.java
@@ -20,7 +20,6 @@ public class AddressService {
 
     public Address createNewAddress(Address address) {
         Address entity = Address.builder()
-                .addressID(address.getAddressID())
                 .publicArea(address.getPublicArea())
                 .addressNumber(address.getAddressNumber())
                 .complement(address.getComplement())
@@ -32,11 +31,11 @@ public class AddressService {
         return addressRepository.save(entity);
     }
 
-    public Address findAddressById(String addressID) {
+    public Address findAddressById(UUID addressID) {
         return addressRepository.findById(addressID).get();
     }
 
-    public Address updateAddress(Address address, String addressID) {
+    public Address updateAddress(Address address, UUID addressID) {
         Address entity = addressRepository.findById(addressID).get();
         entity.setPublicArea(address.getPublicArea());
         entity.setAddressNumber(address.getAddressNumber());
@@ -48,7 +47,7 @@ public class AddressService {
         return addressRepository.save(entity);
     }
 
-    public void deleteAddressById(String addressID) {
+    public void deleteAddressById(UUID addressID) {
         addressRepository.deleteById(addressID);
     }
 }
