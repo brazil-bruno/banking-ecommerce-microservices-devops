0c7394ce0222229c1b90ed8eae725bab1fc7c8a6
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-10 17:22:11 -0300

    String Id changed to UUID

--- ecommerce/order/src/main/java/com/bruno/microservice/order/feignclients/AddressFeignClient.java
+++ ecommerce/order/src/main/java/com/bruno/microservice/order/feignclients/AddressFeignClient.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
+
 @FeignClient(value = "address", url = "${address.url}")
 public interface AddressFeignClient {
 
     @GetMapping("/api/addresses/{addressID}")
-    Address findAddressById(@PathVariable String addressID);
+    Address findAddressById(@PathVariable UUID addressID);
 }
--- ecommerce/order/src/main/java/com/bruno/microservice/order/feignclients/ClientFeignClient.java
+++ ecommerce/order/src/main/java/com/bruno/microservice/order/feignclients/ClientFeignClient.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.UUID;
+
 @FeignClient(value = "client", url = "${client.url}")
 public interface ClientFeignClient {
 
     @GetMapping("/api/clients/{clientID}")
     @ResponseStatus(HttpStatus.OK)
-    Client findClientById(@PathVariable String clientID);
+    Client findClientById(@PathVariable UUID clientID);
 }
