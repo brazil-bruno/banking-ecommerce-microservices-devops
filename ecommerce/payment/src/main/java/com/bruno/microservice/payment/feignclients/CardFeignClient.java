6c839e67297df133d045fdb8bbb649f21080b62a
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-11 11:55:53 -0300

    updateCard added

--- ecommerce/payment/src/main/java/com/bruno/microservice/payment/feignclients/CardFeignClient.java
+++ ecommerce/payment/src/main/java/com/bruno/microservice/payment/feignclients/CardFeignClient.java
@@ -3,9 +3,7 @@ package com.bruno.microservice.payment.feignclients;
 import com.bruno.microservice.payment.entities.Card;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -15,4 +13,8 @@ public interface CardFeignClient {
     @GetMapping("/api/cards/{cardID}")
     @ResponseStatus(HttpStatus.OK)
     Card findCardById(@PathVariable UUID cardID);
+
+    @PutMapping("/api/cards/{cardID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    Card updateCard(@RequestBody Card card, @PathVariable UUID cardID);
 }
