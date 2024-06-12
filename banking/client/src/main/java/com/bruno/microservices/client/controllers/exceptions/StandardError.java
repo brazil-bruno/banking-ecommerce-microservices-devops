2913b1c1dd3dc4bbdce0e91e6fff97d82157f9b0
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-12 10:59:49 -0300

    exceptions created and implemented

---
+++ banking/client/src/main/java/com/bruno/microservices/client/controllers/exceptions/StandardError.java
@@ -0,0 +1,20 @@
+package com.bruno.microservices.client.controllers.exceptions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@NoArgsConstructor
+@Data
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Instant timeStamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+}
--- banking/client/src/main/java/com/bruno/microservices/client/services/ClientService.java
+++ banking/client/src/main/java/com/bruno/microservices/client/services/ClientService.java
@@ -1,14 +1,23 @@
 package com.bruno.microservices.client.services;
 
 import com.bruno.microservices.client.dto.ClientDTO;
+import com.bruno.microservices.client.dto.ClientNewDTO;
 import com.bruno.microservices.client.entities.Address;
 import com.bruno.microservices.client.entities.Client;
 import com.bruno.microservices.client.feignclients.AddressFeignClient;
 import com.bruno.microservices.client.repositories.ClientRepository;
+import com.bruno.microservices.client.services.exceptions.DataBaseException;
+import com.bruno.microservices.client.services.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import javax.persistence.EntityNotFoundException;
+import java.util.Date;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,45 +28,47 @@ public class ClientService {
 
     private final AddressFeignClient addressFeignClient;
 
-    public List<Client> findAllClients() {
-        return clientRepository.findAll();
+    public Page<ClientNewDTO> findAllClients(Pageable pageable) {
+        Page<Client> clients = clientRepository.findAll(pageable);
+        return clients.map(client -> new ClientNewDTO(client));
     }
 
-    public Client createNewClient(ClientDTO clientDTO) {
-        Address address = Address.builder()
-                .publicArea(clientDTO.getPublicArea())
-                .addressNumber(clientDTO.getAddressNumber())
-                .complement(clientDTO.getClientName())
-                .neighborhood(clientDTO.getNeighborhood())
-                .zipCode(clientDTO.getZipCode())
-                .city(clientDTO.getCity())
-                .state(clientDTO.getState())
-                .build();
+    public ClientDTO createNewClient(ClientDTO clientDTO) {
+        Address address = Address.builder().publicArea(clientDTO.getPublicArea()).addressNumber(clientDTO.getAddressNumber()).complement(clientDTO.getClientName()).neighborhood(clientDTO.getNeighborhood()).zipCode(clientDTO.getZipCode()).city(clientDTO.getCity()).state(clientDTO.getState()).build();
         address = addressFeignClient.createNewAddress(address);
 
-        Client client = Client.builder()
-                .clientName(clientDTO.getClientName())
-                .clientEmail(clientDTO.getClientEmail())
-                .clientPhone(clientDTO.getClientPhone())
-                .addressID(address.getAddressID())
-                .build();
-        return clientRepository.save(client);
+        Client client = Client.builder().clientName(clientDTO.getClientName()).clientEmail(clientDTO.getClientEmail()).clientPhone(clientDTO.getClientPhone()).addressID(address.getAddressID()).createdAt(new Date()).build();
+        clientRepository.save(client);
+        return new ClientDTO(client, address);
     }
 
-    public Client findClientById(UUID clientID) {
-        return clientRepository.findById(clientID).get();
+    public ClientNewDTO findClientById(UUID clientID) {
+        Optional<Client> client = clientRepository.findById(clientID);
+        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client id not found!"));
+        return new ClientNewDTO(entity);
     }
 
-    public Client updateClient(Client client, UUID clientID) {
-        Client entity = clientRepository.findById(clientID).get();
-        entity.setClientEmail(client.getClientEmail());
-        entity.setClientPhone(client.getClientPhone());
-        return clientRepository.save(entity);
+    public ClientNewDTO updateClient(ClientNewDTO clientNewDTO, UUID clientID) {
+        try {
+            Client client = clientRepository.getOne(clientID);
+            copyDtoToEntity(clientNewDTO, client);
+            client = clientRepository.save(client);
+            return new ClientNewDTO(client);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Client id not found: " + clientID);
+        }
     }
 
     public void deleteClientById(UUID clientID) {
-        Client entity = clientRepository.findById(clientID).get();
+        Optional<Client> client = clientRepository.findById(clientID);
+        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client id not found!"));
         addressFeignClient.deleteAddressById(entity.getAddressID());
         clientRepository.deleteById(clientID);
+    }
+
+    private void copyDtoToEntity(ClientNewDTO clientNewDTO, Client client) {
+        client.setClientEmail(clientNewDTO.getClientEmail());
+        client.setClientPhone(clientNewDTO.getClientPhone());
+        client.setUpdatedAt(new Date());
     }
 }
