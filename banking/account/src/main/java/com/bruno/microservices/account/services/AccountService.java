23ed18ba7fe72caa35bf7be322604caca6cf2831
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-12 13:05:43 -0300

    exceptions created and implemented

--- banking/account/src/main/java/com/bruno/microservices/account/services/AccountService.java
+++ banking/account/src/main/java/com/bruno/microservices/account/services/AccountService.java
@@ -5,12 +5,18 @@ import com.bruno.microservices.account.entities.Account;
 import com.bruno.microservices.account.entities.Client;
 import com.bruno.microservices.account.feignclients.ClientFeignClient;
 import com.bruno.microservices.account.repositories.AccountRepository;
+import com.bruno.microservices.account.services.exceptions.DataBaseException;
+import com.bruno.microservices.account.services.exceptions.ResourceNotFoundException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import javax.persistence.EntityNotFoundException;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,44 +26,67 @@ public class AccountService {
 
     private final ClientFeignClient clientFeignClient;
 
-    public List<Account> findAllAccounts() {
-        return accountRepository.findAll();
+    public Page<AccountDTO> findAllAccounts(Pageable pageable) {
+        Page<Account> accounts = accountRepository.findAll(pageable);
+        return accounts.map(account -> new AccountDTO(account));
     }
 
-    public Account createNewAccount(AccountDTO accountDTO, UUID clientID) {
-        Client client = clientFeignClient.findClientById(clientID);
-
-        Account entity = Account.builder()
-                .accountNumber(accountNumberGenerate())
-                .accountBalance(0.0)
-                .accountPassword(accountDTO.getAccountPassword())
-                .clientID(client.getClientID())
-                .build();
-        return accountRepository.save(entity);
+    public AccountDTO createNewAccount(AccountDTO accountDTO, UUID clientID) {
+        try {
+            Client client = clientFeignClient.findClientById(clientID);
+            Account entity = Account.builder()
+                    .accountNumber(accountNumberGenerate())
+                    .accountBalance(0.0)
+                    .accountPassword(accountDTO.getAccountPassword())
+                    .clientID(client.getClientID())
+                    .createdAt(new Date())
+                    .build();
+            accountRepository.save(entity);
+            return new AccountDTO(entity);
+        } catch (FeignException e) {
+            throw new ResourceNotFoundException("Client id not found: " + clientID);
+        }
     }
 
-    public Account findAccountById(UUID accountID) {
-        return accountRepository.findById(accountID).get();
+    public AccountDTO findAccountById(UUID accountID) {
+        Optional<Account> account = accountRepository.findById(accountID);
+        Account entity = account.orElseThrow(() -> new ResourceNotFoundException("Account id not found!"));
+        return new AccountDTO(entity);
     }
 
-    public Account updateAccount(Account account, UUID accountID) {
-        Account entity = accountRepository.findById(accountID).get();
-        entity.setAccountPassword(account.getAccountPassword());
-        return accountRepository.save(entity);
+    public AccountDTO updateAccount(AccountDTO accountDTO, UUID accountID) {
+        try {
+            Account account = accountRepository.getOne(accountID);
+            copyDtoToEntity(accountDTO, account);
+            account = accountRepository.save(account);
+            return new AccountDTO(account);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Account id not found: " + accountID);
+        }
     }
 
     public void deleteAccountById(UUID accountID) {
-        accountRepository.deleteById(accountID);
+        try {
+            accountRepository.deleteById(accountID);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Address id not found: " + accountID);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation!.");
+        }
     }
 
     private String accountNumberGenerate() {
         Random rand = new Random();
         String accountNumber = "0462";
-        for (int i = 0; i < 8; i++)
-        {
+        for (int i = 0; i < 8; i++) {
             int n = rand.nextInt(10) + 0;
             accountNumber += Integer.toString(n);
         }
         return accountNumber;
+    }
+
+    private void copyDtoToEntity(AccountDTO accountDTO, Account account) {
+        account.setAccountPassword(accountDTO.getAccountPassword());
+        account.setUpdatedAt(new Date());
     }
 }
