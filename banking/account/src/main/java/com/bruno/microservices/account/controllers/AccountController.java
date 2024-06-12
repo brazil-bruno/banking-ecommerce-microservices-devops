b862d0fa5b4cd741ed2e895f2a16a9e20ef6c13f
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-12 13:04:48 -0300

    AccountController adjusted

--- banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountController.java
+++ banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountController.java
@@ -4,9 +4,12 @@ import com.bruno.microservices.account.dto.AccountDTO;
 import com.bruno.microservices.account.entities.Account;
 import com.bruno.microservices.account.services.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,26 +22,26 @@ public class AccountController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Account> findAllAccounts() {
-        return accountService.findAllAccounts();
+    public Page<AccountDTO> findAllAccounts(Pageable pageable) {
+        return accountService.findAllAccounts(pageable);
     }
 
-    @PostMapping("client-id/{clientID}")
+    @PostMapping("/client-id/{clientID}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Account createNewAccount(@RequestBody AccountDTO accountDTO, @PathVariable UUID clientID) {
+    public AccountDTO createNewAccount(@RequestBody @Valid AccountDTO accountDTO, @PathVariable UUID clientID) {
         return accountService.createNewAccount(accountDTO, clientID);
     }
 
     @GetMapping("/{accountID}")
     @ResponseStatus(HttpStatus.OK)
-    public Account findAccountById(@PathVariable UUID accountID) {
+    public AccountDTO findAccountById(@PathVariable UUID accountID) {
         return accountService.findAccountById(accountID);
     }
 
     @PutMapping("/{accountID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Account updateAccount(@RequestBody Account account, @PathVariable UUID accountID) {
-        return accountService.updateAccount(account, accountID);
+    public AccountDTO updateAccount(@RequestBody @Valid AccountDTO accountDTO, @PathVariable UUID accountID) {
+        return accountService.updateAccount(accountDTO, accountID);
     }
 
     @DeleteMapping("/{accountID}")
