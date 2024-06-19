79e836043b84e07444d9c8536cc4d210ec0e7418
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 16:07:25 -0300

    findAddressByClientID created

--- banking/address/src/main/java/com/bruno/microservices/address/repositories/AddressRepository.java
+++ banking/address/src/main/java/com/bruno/microservices/address/repositories/AddressRepository.java
@@ -4,8 +4,10 @@ import com.bruno.microservices.address.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
+    Optional<Address> findAddressByClientID(UUID clientID);
 }
