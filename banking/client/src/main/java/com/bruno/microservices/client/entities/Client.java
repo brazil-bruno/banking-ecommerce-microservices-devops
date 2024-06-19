1fae1fbafa5f3187f2b79c95113ae4d479d8cc7b
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 16:39:10 -0300

    ManyToMany roles added, Client Entity refactored

--- banking/client/src/main/java/com/bruno/microservices/client/entities/Client.java
+++ banking/client/src/main/java/com/bruno/microservices/client/entities/Client.java
@@ -1,15 +1,20 @@
 package com.bruno.microservices.client.entities;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_client")
@@ -22,20 +27,27 @@ public class Client implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Type(type = "uuid-char")
     private UUID clientID;
 
     private String clientName;
 
     @Column(unique = true)
-    private String clientEmail;
+    private String email;
 
     private String clientPhone;
 
-    private UUID addressID;
+    private String password;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createdAt;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updatedAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_client_role",
+            joinColumns = @JoinColumn(name = "client_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
