d76a85a16db95c56e982dbc222fd7e53961d1c59
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 15:45:13 -0300

    AccountTransactionController: findAllTransactionsByAccountId method created, deposit and withdraw changed to POST and TransactionDTO

--- banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountTransactionController.java
+++ banking/account/src/main/java/com/bruno/microservices/account/controllers/AccountTransactionController.java
@@ -1,11 +1,14 @@
 package com.bruno.microservices.account.controllers;
 
+import com.bruno.microservices.account.dto.TransactionDTO;
 import com.bruno.microservices.account.entities.Account;
 import com.bruno.microservices.account.services.AccountTransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -14,21 +17,27 @@ import java.util.UUID;
 public class AccountTransactionController {
     private final AccountTransactionService accountTransactionService;
 
-    @PutMapping("/deposit/{accountID}")
+    @PostMapping("/deposit/{accountID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deposit(@RequestBody Account account, @PathVariable UUID accountID) {
-        accountTransactionService.deposit(account, accountID);
+    public void deposit(@RequestBody @Valid TransactionDTO transactionDTO, @PathVariable UUID accountID) {
+        accountTransactionService.deposit(transactionDTO, accountID);
     }
 
-    @PutMapping("/withdraw/{accountID}")
+    @PostMapping("/withdraw/{accountID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void withdraw(@RequestBody Account account, @PathVariable UUID accountID) {
-        accountTransactionService.withdraw(account, accountID);
+    public void withdraw(@RequestBody @Valid TransactionDTO transactionDTO, @PathVariable UUID accountID) {
+        accountTransactionService.withdraw(transactionDTO, accountID);
     }
 
     @GetMapping("/balance/{accountID}")
     @ResponseStatus(HttpStatus.OK)
     public double balance(@PathVariable UUID accountID) {
         return accountTransactionService.balance(accountID);
+    }
+
+    @GetMapping(value = "/find-transactions-by-account-id/{accountID}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TransactionDTO> findAllTransactionsByAccountId(@PathVariable UUID accountID) {
+        return accountTransactionService.findAllTransactionsByAccountId(accountID);
     }
 }
