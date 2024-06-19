f966b57b3f476c8c733446cf98bbeba47146110a
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 17:08:44 -0300

    ClientController refactored

--- banking/client/src/main/java/com/bruno/microservices/client/controllers/ClientController.java
+++ banking/client/src/main/java/com/bruno/microservices/client/controllers/ClientController.java
@@ -1,49 +1,60 @@
 package com.bruno.microservices.client.controllers;
 
-import com.bruno.microservices.client.dto.ClientDTO;
-import com.bruno.microservices.client.dto.ClientNewDTO;
+import com.bruno.microservices.client.dto.ClientAddressDTO;
+import com.bruno.microservices.client.dto.ClientInsertDTO;
+import com.bruno.microservices.client.dto.ClientUpdateDTO;
+import com.bruno.microservices.client.entities.Client;
 import com.bruno.microservices.client.services.ClientService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.UUID;
 
+import com.bruno.microservices.client.dto.ClientDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/api/clients")
+@RequestMapping(value = "/api/clients")
 @RequiredArgsConstructor
 public class ClientController {
 
     private final ClientService clientService;
 
-    @GetMapping
+    @GetMapping(value = "/find-all")
     @ResponseStatus(HttpStatus.OK)
-    public Page<ClientNewDTO> findAllClients(Pageable pageable) {
+    public Page<ClientDTO> findAllClients(Pageable pageable) {
         return clientService.findAllClients(pageable);
     }
 
-    @PostMapping
+    @PostMapping(value = "/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public ClientDTO createNewClient(@RequestBody @Valid ClientDTO clientDTO) {
-        return clientService.createNewClient(clientDTO);
+    public ClientDTO createNewClient(@RequestBody @Valid ClientInsertDTO clientInsertDTO) {
+        return clientService.createNewClient(clientInsertDTO);
     }
 
-    @GetMapping("/{clientID}")
+    @GetMapping(value = "/find-by-id/{clientID}")
     @ResponseStatus(HttpStatus.OK)
-    public ClientNewDTO findClientById(@PathVariable UUID clientID) {
+    public ClientAddressDTO findClientById(@PathVariable UUID clientID) {
         return clientService.findClientById(clientID);
     }
 
-    @PutMapping("/{clientID}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ClientNewDTO updateClient(@RequestBody @Valid ClientNewDTO clientNewDTO, @PathVariable UUID clientID) {
-        return clientService.updateClient(clientNewDTO, clientID);
+    @GetMapping(value = "/find-by-email")
+    @ResponseStatus(HttpStatus.OK)
+    public Client findClientByEmail(@RequestParam String email) {
+        return clientService.findClientByEmail(email);
     }
 
-    @DeleteMapping("/{clientID}")
+    @PutMapping(value = "/update/{clientID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ClientDTO updateClient(@RequestBody @Valid ClientUpdateDTO clientUpdateDTO, @PathVariable UUID clientID) {
+        return clientService.updateClient(clientUpdateDTO, clientID);
+    }
+
+    @DeleteMapping(value = "/delete/{clientID}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteClientById(@PathVariable UUID clientID) {
         clientService.deleteClientById(clientID);
