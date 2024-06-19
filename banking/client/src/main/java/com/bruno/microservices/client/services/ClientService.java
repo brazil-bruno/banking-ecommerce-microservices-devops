2eca074c8aecedc965f1870e93adea8f7c112399
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 17:08:19 -0300

    ClientService refactored and BCryptPasswordEncoder added

--- banking/client/src/main/java/com/bruno/microservices/client/services/ClientService.java
+++ banking/client/src/main/java/com/bruno/microservices/client/services/ClientService.java
@@ -1,74 +1,111 @@
 package com.bruno.microservices.client.services;
 
-import com.bruno.microservices.client.dto.ClientDTO;
-import com.bruno.microservices.client.dto.ClientNewDTO;
+import com.bruno.microservices.client.dto.*;
 import com.bruno.microservices.client.entities.Address;
 import com.bruno.microservices.client.entities.Client;
+import com.bruno.microservices.client.entities.Role;
 import com.bruno.microservices.client.feignclients.AddressFeignClient;
 import com.bruno.microservices.client.repositories.ClientRepository;
-import com.bruno.microservices.client.services.exceptions.DataBaseException;
+import com.bruno.microservices.client.repositories.RoleRepository;
 import com.bruno.microservices.client.services.exceptions.ResourceNotFoundException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Date;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
 
-    private final ClientRepository clientRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private final AddressFeignClient addressFeignClient;
 
-    public Page<ClientNewDTO> findAllClients(Pageable pageable) {
+    private final ClientRepository clientRepository;
+
+    private final RoleRepository roleRepository;
+
+    public Page<ClientDTO> findAllClients(Pageable pageable) {
         Page<Client> clients = clientRepository.findAll(pageable);
-        return clients.map(client -> new ClientNewDTO(client));
+        return clients.map(client -> new ClientDTO(client));
     }
 
-    public ClientDTO createNewClient(ClientDTO clientDTO) {
-        Address address = Address.builder().publicArea(clientDTO.getPublicArea()).addressNumber(clientDTO.getAddressNumber()).complement(clientDTO.getClientName()).neighborhood(clientDTO.getNeighborhood()).zipCode(clientDTO.getZipCode()).city(clientDTO.getCity()).state(clientDTO.getState()).build();
-        address = addressFeignClient.createNewAddress(address);
-
-        Client client = Client.builder().clientName(clientDTO.getClientName()).clientEmail(clientDTO.getClientEmail()).clientPhone(clientDTO.getClientPhone()).addressID(address.getAddressID()).createdAt(new Date()).build();
-        clientRepository.save(client);
-        return new ClientDTO(client, address);
+    public ClientDTO createNewClient(ClientInsertDTO clientInsertDTO) {
+        Client client = new Client();
+        copyDtoToEntityNew(clientInsertDTO, client);
+        client.setPassword(bCryptPasswordEncoder.encode(clientInsertDTO.getPassword()));
+        client.setCreatedAt(new Date());
+        client = clientRepository.save(client);
+        return new ClientDTO(client);
     }
 
-    public ClientNewDTO findClientById(UUID clientID) {
+    public ClientAddressDTO findClientById(UUID clientID) {
         Optional<Client> client = clientRepository.findById(clientID);
-        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client id not found!"));
-        return new ClientNewDTO(entity);
+        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client not found!"));
+        try {
+            Address address = addressFeignClient.findAddressByClientId(entity.getClientID());
+            return new ClientAddressDTO(entity, address);
+        } catch (FeignException e) {
+            throw new ResourceNotFoundException("Address not found");
+        }
     }
 
-    public ClientNewDTO updateClient(ClientNewDTO clientNewDTO, UUID clientID) {
+    public Client findClientByEmail(String email) {
+        Client client = clientRepository.findByEmail(email);
+        return client;
+    }
+
+    public ClientDTO updateClient(ClientUpdateDTO clientUpdateDTO, UUID clientID) {
         try {
             Client client = clientRepository.getOne(clientID);
-            copyDtoToEntity(clientNewDTO, client);
+            copyDtoToEntity(clientUpdateDTO, client);
+            client.setPassword(bCryptPasswordEncoder.encode(clientUpdateDTO.getPassword()));
             client = clientRepository.save(client);
-            return new ClientNewDTO(client);
+            return new ClientDTO(client);
         } catch (EntityNotFoundException e) {
-            throw new ResourceNotFoundException("Client id not found: " + clientID);
+            throw new ResourceNotFoundException("Client not found");
         }
     }
 
     public void deleteClientById(UUID clientID) {
         Optional<Client> client = clientRepository.findById(clientID);
-        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client id not found!"));
-        addressFeignClient.deleteAddressById(entity.getAddressID());
-        clientRepository.deleteById(clientID);
+        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client not found!"));
+        try {
+            addressFeignClient.deleteAddressByClientId(entity.getClientID());
+        } catch (FeignException e) {
+            throw new ResourceNotFoundException("Address not found");
+        }
+        clientRepository.deleteById(entity.getClientID());
     }
 
-    private void copyDtoToEntity(ClientNewDTO clientNewDTO, Client client) {
-        client.setClientEmail(clientNewDTO.getClientEmail());
-        client.setClientPhone(clientNewDTO.getClientPhone());
+    private void copyDtoToEntity(ClientDTO clientDTO, Client client) {
+        client.setEmail(clientDTO.getEmail());
+        client.setClientPhone(clientDTO.getClientPhone());
         client.setUpdatedAt(new Date());
+
+        client.getRoles().clear();
+        for (RoleDTO roleDTO : clientDTO.getRoles()) {
+            Role role = roleRepository.getOne(roleDTO.getRoleID());
+            client.getRoles().add(role);
+        }
+    }
+
+    private void copyDtoToEntityNew(ClientDTO clientDTO, Client client) {
+        client.setClientName(clientDTO.getClientName());
+        client.setEmail(clientDTO.getEmail());
+        client.setClientPhone(clientDTO.getClientPhone());
+
+        client.getRoles().clear();
+        for (RoleDTO roleDTO : clientDTO.getRoles()) {
+            Role role = roleRepository.getOne(roleDTO.getRoleID());
+            client.getRoles().add(role);
+        }
     }
 }
+
+
