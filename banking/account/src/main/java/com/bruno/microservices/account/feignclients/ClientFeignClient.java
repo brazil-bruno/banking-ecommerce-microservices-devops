6dbcaa4d44bc7bf6640ab42fa05e3540dafffe14
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 15:58:34 -0300

    path changed and ResponseStatus added

--- banking/account/src/main/java/com/bruno/microservices/account/feignclients/ClientFeignClient.java
+++ banking/account/src/main/java/com/bruno/microservices/account/feignclients/ClientFeignClient.java
@@ -2,14 +2,16 @@ package com.bruno.microservices.account.feignclients;
 
 import com.bruno.microservices.account.entities.Client;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 
 @FeignClient(value = "client", url = "${client.url}")
 public interface ClientFeignClient {
-
-    @GetMapping("/api/clients/{clientID}")
+    @GetMapping("/api/clients/find-by-id/{clientID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     Client findClientById(@PathVariable UUID clientID);
 }
