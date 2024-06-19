ee8df438bd463920a9d910b92d36a4feb4fa992d
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-19 15:36:20 -0300

    TransactionDTO created

---
+++ banking/account/src/main/java/com/bruno/microservices/account/dto/TransactionDTO.java
@@ -0,0 +1,36 @@
+package com.bruno.microservices.account.dto;
+
+import com.bruno.microservices.account.entities.Transaction;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.Date;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TransactionDTO {
+
+    private UUID transactionID;
+
+    @Positive(message = "Must be positive")
+    @NotNull(message = "Required field!")
+    private double amount;
+
+    private double balance;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createdAt;
+
+    public TransactionDTO(Transaction transaction) {
+        this.transactionID = transaction.getTransactionID();
+        this.amount = transaction.getAmount();
+        this.balance = transaction.getBalance();
+        this.createdAt = transaction.getCreatedAt();
+    }
+}
