2d6f1d2431ce1b6af8e0bac4863f81ddaf721216
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-20 09:03:16 -0300

    NotNull added

--- banking/address/src/main/java/com/bruno/microservices/address/dto/AddressDTO.java
+++ banking/address/src/main/java/com/bruno/microservices/address/dto/AddressDTO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
@@ -39,6 +40,7 @@ public class AddressDTO implements Serializable {
     @NotEmpty(message = "Required field!")
     private String state;
 
+    @NotNull(message = "Required field!")
     private UUID clientID;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
