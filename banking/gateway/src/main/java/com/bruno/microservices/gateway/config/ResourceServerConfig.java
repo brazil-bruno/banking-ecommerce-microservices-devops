203283d33dfe6e0722abd52322ca8d9b94c6be6a
brazil-bruno <bruno.brazil2022@gmail.com>
2024-06-20 09:09:44 -0300

    AppConfig: jwt secret, token converter and token store implemented. ResourceServerConfig: ResourceServerSecurityConfigurer, HttpSecurity, CorsConfigurationSource and FilterRegistrationBean implemented

---
+++ banking/gateway/src/main/java/com/bruno/microservices/gateway/config/ResourceServerConfig.java
@@ -0,0 +1,84 @@
+package com.bruno.microservices.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = {"/oauth/oauth/token", "/account/api/transactions/**", "/h2-console/**"};
+
+    private static final String[] CLIENT = {
+            "/client/api/clients/find-by-id/**",
+            "/address/api/addresses/find-address-by-client-id/**",
+            "/address/api/addresses/update/**",
+            "/client/api/clients/update/**",
+            "/account/api/accounts/find-by-id/**",
+            "/account/api/accounts/update/**",
+            "/account/api/transactions/find-transactions-by-account-id/**",
+    };
+
+    private static final String[] ADMIN = {"/address/**", "/client/**", "/account/**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, CLIENT).hasAnyRole("CLIENT", "ADMIN")
+                .antMatchers(HttpMethod.PUT, CLIENT).hasAnyRole("CLIENT", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
+}
